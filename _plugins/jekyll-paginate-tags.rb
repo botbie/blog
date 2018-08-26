edf64d928183110a37ae8abb101d0d493f277961
nguquen <thienlduc@gmail.com>
2018-08-26 15:55:15 +0700

    Fix Jekyll paginate plugin

--- _plugins/jekyll-paginate-tags.rb
+++ _plugins/jekyll-paginate-tags.rb
@@ -4,9 +4,74 @@ module Jekyll
   module Paginate
     module Tags
 
-      # Internal requires
-      autoload :TagPage, 'jekyll-paginate-tag/tag-page'
-      autoload :Pager, 'jekyll-paginate-tag/pager'
+      class TagPage < Jekyll::Page
+        # Attributes for Liquid templates
+        ATTRIBUTES_FOR_LIQUID = %w(
+          tag
+          content
+          dir
+          name
+          path
+          url
+        )
+
+        # Initialize a new Page.
+        #
+        # site - The Site object.
+        # base - The String path to the source.
+        # dir  - The String path between the source and the file.
+        # name - The String filename of the file.
+        # tag - The String tag
+        def initialize(site, base, tag)
+          layout = site.config['paginate_tag_layout'] || 'tags.html'
+          super(site, base, '_layouts', layout)
+          process('index.html')
+
+          # Get the tag into layout using page.tag
+          @tag = tag
+        end
+
+        # Produce a tag object suitable for Liquid.
+        #
+        # Returns a String
+        def tag
+          if @tag.is_a? String
+            @tag
+          end
+        end
+      end
+
+      class Pager < Jekyll::Paginate::Pager
+        # Update paginator.previous_page_path and next_page_path to add tag path
+        #
+        # site            - the Jekyll::Site object
+        # tag_path   - tag path, eg /tag/web/
+        #
+        # Returns nothing.
+        def update_paginate_paths(site, tag_path)
+          if @page > 1
+            @previous_page_path = tag_path.sub(/(\/)+$/,'') + @previous_page_path
+          end
+          if @page < @total_pages
+            @next_page_path = tag_path.sub(/(\/)+$/,'') + @next_page_path
+          end
+        end
+
+        # Static: Return the pagination path of the page
+        #
+        # site     - the Jekyll::Site object
+        # num_page - the pagination page number
+        # paginate_path - the explicit paginate path, if provided
+        #
+        # Returns the pagination path as a string
+        def self.paginate_path_tag(site, num_page, tag_path, paginate_path = site.config['paginate_path'])
+          return nil if num_page.nil?
+          return tag_path if num_page <= 1
+          format = tag_path.sub(/(\/)+$/,'') + paginate_path
+          format = format.sub(':num', num_page.to_s)
+          ensure_leading_slash(format)
+        end
+      end
 
       # Per-tag pagination.
       # Based on jekyll-paginate.
